892f19cb512c81d7f559951d26769505bc3b3b52
martagdvalle <martagdvalle@outlook.es>
2022-12-10 14:28:46 +0100

    new

--- recipe_api/routes.py
+++ recipe_api/routes.py
@@ -3,13 +3,8 @@ from recipe_api import db, app
 from recipe_api.models import Recipe
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/recipes', methods=['POST'])
+@app.route('/', methods=['POST'])
 def create_recipe():
-    
     name = request.json['name']
     ingredients = request.json['ingredients']
     steps = request.json['steps']
@@ -20,17 +15,17 @@ def create_recipe():
     db.session.commit()
     return format_recipe(recipe)
 
-@app.route('/recipes', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_recipes():
     recipes = Recipe.query.all()
     return {'recipes': [format_recipe(recipe) for recipe in recipes]}
 
-@app.route('/recipes/<int:id>', methods=['GET'])
+@app.route('/<int:id>', methods=['GET'])
 def get_recipe(id):
     recipe = Recipe.query.get(id)
     return format_recipe(recipe)
 
-@app.route('/recipes/<int:id>', methods=['PUT'])
+@app.route('/<int:id>', methods=['PUT'])
 def update_recipe(id):
     recipe = Recipe.query.get(id)
     recipe.name = request.json['name']
